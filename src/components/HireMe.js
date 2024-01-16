798512db15045fc86f197c982dd4fa8f3a0527bf
Jay Patel <jayu.patel0234@gmail.com>
2024-01-16 08:43:11 -0700

    Bug-fix 2

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const HireMe = () => {
   return (
-    <div className='fixed left-1 bottom-1 flex items-center justify-center overflow-hidden lg:right-auto lg:left-[270px] lg:top-0 lg:bottom-[625px] lg:absolute md;left-[235px]'>
+    <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-2 md:left-auto md:top-0 md:bottom-[625px] md:absolute xs:right-0'>
 
     <div className='w-64 h-auto flex items-center justify-center relative md:w-32'>
         <CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
@@ -12,8 +12,7 @@ const HireMe = () => {
         <Link href="mailto:jayusp.ca0234@gmail.com" className='flex items-center justify-center 
         absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
          bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full
-        font-semibold text-base hover:bg-light  hover:text-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
-        md:w-10 md:h-10 md:text-[10px]
+        font-semibold text-base hover:bg-light  hover:text-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-10 md:h-10 md:text-[10px]
         '>Hire Me</Link>
     </div>
     </div>

c2f3c5fa70bd9974c4ee0d7c0fe8ce73b9813cc9
Jay Patel <jayu.patel0234@gmail.com>
2024-01-20 21:57:03 -0700

    Detailes edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -72,14 +72,14 @@ const about = () => {
                         <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
                             <div className='flex flex-col items-end justify-center xl:items-center'>
                                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                                    <AnimatedNumbers value={5} />+
+                                    <AnimatedNumbers value={1} />+
                                 </span>
                                 <h2 className='=text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>satisfied clients</h2>
                             </div>
 
                             <div className='flex flex-col items-end justify-center xl:items-center'>
                                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
-                                    <AnimatedNumbers value={4} />+
+                                    <AnimatedNumbers value={5} />+
                                 </span>
                                 <h2 className='=text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>projects completed</h2>
                             </div>
@@ -88,7 +88,7 @@ const about = () => {
                                 <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                                     <AnimatedNumbers value={3} />+
                                 </span>
-                                <h2 className='=text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>years of experience</h2>
+                                <h2 className='=text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>years of learning</h2>
                             </div>
                         </div>
 

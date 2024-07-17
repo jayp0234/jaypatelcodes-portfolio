dd90bd7921662afee1f34c54759a1456ed2288d8
Jay Patel <jayu.patel0234@gmail.com>
2024-07-17 02:32:50 -0600

    Update experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -39,17 +39,34 @@ const Experience = () => {
         style={{scaleY: scrollYProgress}}
         className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]'/>
 <ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2 '>
-   <Details className ="text-[#2761ae] underline decoration-company decoration-4 drop-shadow-lg" position="Full Stack Developer Intern" company="Sys Creations Inc." time="May 2023 - Aug 2023" address="Burlington, ON" 
-   work="Contributed to the development of a dynamic web application with a 43% increase in user engagement, using React.
-   js, TypeScript, and MongoDB. Enhanced application functionality by integrating RESTful APIs using Spring Boot. Participated in deploying automated testing using Docker and continuous integration processes to ensure optimal
-   system performance. Assisted in developing a comprehensive appointment scheduling system and implemented best practices for responsive
-   design and API security, prioritizing data privacy and user safety." 
-   companyLink="https://www.syscreations.com/" />
+   <Details className ="text-[#f97316] underline decoration-company decoration-4 drop-shadow-lg" position="Web Developer Intern" company="Dolphine Software Technologies" time="Nov 2023 - May 2024" address="Remote" 
+   work="Collaborated with a team of 6 developers to design and implement core functionalities of a telehealth application, using
+JavaScript, React Native, and Node.js. Worked with payment gateway APIs such as Stripe and PayPal to facilitate secure transactions for health consultations and services, processing over $35,000 in transactions monthly. Successfully managed project tasks and timelines, ensuring timely delivery and meeting all project milestones. Documented technical specifications, API endpoints, and user guides to facilitate team collaboration and future maintenance." 
+   companyLink="https://dolphinsoftwaretechnologies.com/" />
 
 
 </ul>
 
+
     </div>
+    <div ref={ref} className='w-[75%] mx-auto relative lg:w-[90%] md:w-full'>
+
+<motion.div
+style={{scaleY: scrollYProgress}}
+className='absolute left-9 top-4 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]'/>
+<ul className='w-full flex flex-col items-start justify-between ml-4 xs:ml-2 '>
+<Details className ="text-[#2761ae] underline decoration-company decoration-4 drop-shadow-lg" position="Software Developer Intern" company="Sys Creations Inc." time="May 2023 - Aug 2023" address="Burlington, ON" 
+work="Contributed to the development of a dynamic web application with a 43% increase in user engagement, using React.
+js, TypeScript, and MongoDB. Enhanced application functionality by integrating RESTful APIs using Spring Boot. Participated in deploying automated testing using Docker and continuous integration processes to ensure optimal
+system performance. Assisted in developing a comprehensive appointment scheduling system and implemented best practices for responsive
+design and API security, prioritizing data privacy and user safety." 
+companyLink="https://www.syscreations.com/" />
+
+
+</ul>
+
+
+</div>
     </div>
   )
 }
